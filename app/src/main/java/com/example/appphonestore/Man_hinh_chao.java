1b65299237b0ddf5c8c11caaefdc00743f06bd78
Nguyễn Văn Đạt <94579455+datnvph14454@users.noreply.github.com>
2022-03-31 16:13:38 +0700

    dat

---
+++ app/src/main/java/com/example/appphonestore/Man_hinh_chao.java
@@ -0,0 +1,14 @@
+package com.example.appphonestore;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class Man_hinh_chao extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_man_hinh_chao);
+    }
+}
