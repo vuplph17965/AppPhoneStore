e762d3135d87147308b077e2b1e2269012586e47
Nguyễn Văn Đạt <94579455+datnvph14454@users.noreply.github.com>
2022-04-02 14:33:28 +0700

    dat

--- app/src/main/java/com/example/appphonestore/Man_hinh_chao.java
+++ app/src/main/java/com/example/appphonestore/Man_hinh_chao.java
@@ -2,7 +2,9 @@ package com.example.appphonestore;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.os.Handler;
 
 public class Man_hinh_chao extends AppCompatActivity {
 
@@ -10,5 +12,11 @@ public class Man_hinh_chao extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_man_hinh_chao);
+        new Handler().postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                startActivity(new Intent(getApplicationContext(), LoginActivity.class));
+            }
+        },3500);
     }
 }
