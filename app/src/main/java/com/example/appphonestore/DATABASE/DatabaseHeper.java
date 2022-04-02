43d47fad760de030de4bf13c8506a09933475e61
GIGABYTE <vuplph17965@fpt.edu.vn>
2022-04-02 15:09:30 +0700

    AppPhoneStore

--- app/src/main/java/com/example/appphonestore/DATABASE/DatabaseHeper.java
+++ app/src/main/java/com/example/appphonestore/DATABASE/DatabaseHeper.java
@@ -1,4 +1,24 @@
 package com.example.appphonestore.DATABASE;
 
-public class DatabaseHeper {
+import android.content.Context;
+import android.database.sqlite.SQLiteDatabase;
+import android.database.sqlite.SQLiteOpenHelper;
+
+
+public class DatabaseHeper extends SQLiteOpenHelper {
+
+
+    public DatabaseHeper(Context context) {
+        super(context, "PHONESTORE", null, 1);
+    }
+
+    @Override
+    public void onCreate(SQLiteDatabase sqLiteDatabase) {
+ 
+    }
+
+    @Override
+    public void onUpgrade(SQLiteDatabase sqLiteDatabase, int i, int i1) {
+
+    }
 }
