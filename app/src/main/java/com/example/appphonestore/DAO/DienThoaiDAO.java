78bd5878b4a46648c2a8cc088255da4430558317
Nguyễn Văn Đạt <94579455+datnvph14454@users.noreply.github.com>
2022-04-05 15:25:43 +0700

    dat

--- app/src/main/java/com/example/appphonestore/DAO/DienThoaiDAO.java
+++ app/src/main/java/com/example/appphonestore/DAO/DienThoaiDAO.java
@@ -1,4 +1,52 @@
 package com.example.appphonestore.DAO;
 
+import android.content.ContentValues;
+import android.content.Context;
+import android.database.Cursor;
+import android.database.sqlite.SQLiteDatabase;
+
+import com.example.appphonestore.DATABASE.DatabaseHeper;
+import com.example.appphonestore.MODEL.DienThoai;
+
+import java.util.ArrayList;
+import java.util.List;
+
 public class DienThoaiDAO {
+    SQLiteDatabase sqLiteDatabase;
+    DatabaseHeper createData;
+
+    public DienThoaiDAO(Context context) {
+        createData = new DatabaseHeper(context);
+        sqLiteDatabase = createData.getWritableDatabase();
+    }
+
+    public long ADDS(DienThoai dt) {
+        ContentValues values = new ContentValues();
+        values.put(DienThoai.COL_NAME_MALS, dt.getMas());
+        values.put(DienThoai.COL_NAME_TenS, dt.getTens());
+        values.put(DienThoai.COL_NAME_GIAS, dt.getGias());
+        values.put(DienThoai.COL_NAME_SOLUONGs, dt.getSl());
+        values.put(DienThoai.COL_NAME_NGAYNHAP, dt.getNgaynhap());
+        values.put(DienThoai.COL_NAME_NOISX, dt.getNoiSX());
+        return sqLiteDatabase.insert(DienThoai.TB_NAME, null, values);
+    }
+
+    public int DELETES(DienThoai dt) {
+        return sqLiteDatabase.delete(DienThoai.TB_NAME, "maDT=?", new String[]{String.valueOf(dt.getMas())});
+    }
+
+    public int UPDATES(DienThoai dt) {
+        ContentValues values = new ContentValues();
+        values.put(DienThoai.COL_NAME_MALS, dt.getMas());
+        values.put(DienThoai.COL_NAME_TenS, dt.getTens());
+        values.put(DienThoai.COL_NAME_GIAS, dt.getGias());
+        values.put(DienThoai.COL_NAME_SOLUONGs, dt.getSl());
+        values.put(DienThoai.COL_NAME_NGAYNHAP, dt.getNgaynhap());
+        values.put(DienThoai.COL_NAME_NOISX, dt.getNoiSX());
+        return sqLiteDatabase.update(DienThoai.TB_NAME, values, "maDT=?", new String[]{String.valueOf(dt.getMas())});
+    }
+
+
+
+
 }
