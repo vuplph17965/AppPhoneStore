6293105dfe1a850f93985f372a9cf43cd5709db6
GIGABYTE <vuplph17965@fpt.edu.vn>
2022-04-05 15:28:43 +0700

    AppPhoneStore

--- app/src/main/java/com/example/appphonestore/DAO/DienThoaiDAO.java
+++ app/src/main/java/com/example/appphonestore/DAO/DienThoaiDAO.java
@@ -1,4 +1,28 @@
 package com.example.appphonestore.DAO;
 
+import android.content.ContentValues;
+import android.content.Context;
+import android.database.sqlite.SQLiteDatabase;
+
+import com.example.appphonestore.DATABASE.DatabaseHeper;
+import com.example.appphonestore.MODEL.DienThoai;
+
 public class DienThoaiDAO {
+
+    SQLiteDatabase database;
+
+    public DienThoaiDAO(Context context) {
+        DatabaseHeper databaseHeper = new DatabaseHeper(context);
+        database = databaseHeper.getWritableDatabase();
+    }
+
+    public long Add(DienThoai dienThoai) {
+        ContentValues values = new ContentValues();
+        values.put(DienThoai.COL_NAME_MAS, dienThoai.getMaDT());
+        values.put(DienThoai.COL_NAME_TenS, dienThoai.getTenDT());
+        values.put(DienThoai.COL_NAME_GIAS, dienThoai.getGiaDT());
+        values.put(DienThoai.COL_NAME_SOLUONGs, dienThoai.getSl());
+        values.put(DienThoai.COL_NAME_NGAYNHAP, dienThoai.getNgaynhap());
+        values.put(DienThoai.);
+    }
 }
--- app/src/main/java/com/example/appphonestore/LoginActivity.java
+++ app/src/main/java/com/example/appphonestore/LoginActivity.java
@@ -3,12 +3,23 @@ package com.example.appphonestore;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.Button;
+import android.widget.CheckBox;
+import android.widget.EditText;
 
 public class LoginActivity extends AppCompatActivity {
-
+    EditText ed_user, ed_pass;
+    Button btn_login, btn_exit;
+    CheckBox chk_remember;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
+        setTitle("LOGIN");
+        ed_user = findViewById(R.id.ed_user);
+        ed_pass = findViewById(R.id.ed_user);
+        btn_login = findViewById(R.id.btn_login);
+        btn_exit = findViewById(R.id.btn_exit);
+        chk_remember = findViewById(R.id.chk_remember);
     }
 }
