ee014893878b2c3a070e412107958f408166e94b
GIGABYTE <vuplph17965@fpt.edu.vn>
2022-04-07 14:27:23 +0700

    AppPhoneStore

--- app/src/main/java/com/example/appphonestore/MODEL/DienThoai.java
+++ app/src/main/java/com/example/appphonestore/MODEL/DienThoai.java
@@ -1,17 +1,17 @@
 package com.example.appphonestore.MODEL;
 
 public class DienThoai {
-    int maDT;
-    String tenDT;
-    int giaDT;
+    int mas;
+    String tens;
+    int gias;
     int sl;
     String ngaynhap;
     String noiSX;
 
-    public DienThoai(int maDT, String tenDT, int giaDT, int sl, String ngaynhap, String noiSX) {
-        this.maDT = maDT;
-        this.tenDT = tenDT;
-        this.giaDT = giaDT;
+    public DienThoai(int mas, String tens, int gias, int sl, String ngaynhap, String noiSX) {
+        this.mas = mas;
+        this.tens = tens;
+        this.gias = gias;
         this.sl = sl;
         this.ngaynhap = ngaynhap;
         this.noiSX = noiSX;
@@ -20,28 +20,28 @@ public class DienThoai {
     public DienThoai() {
     }
 
-    public int getMaDT() {
-        return maDT;
+    public int getMas() {
+        return mas;
     }
 
-    public void setMaDT(int maDT) {
-        this.maDT = maDT;
+    public void setMas(int mas) {
+        this.mas = mas;
     }
 
-    public String getTenDT() {
-        return tenDT;
+    public String getTens() {
+        return tens;
     }
 
-    public void setTenDT(String tenDT) {
-        this.tenDT = tenDT;
+    public void setTens(String tens) {
+        this.tens = tens;
     }
 
-    public int getGiaDT() {
-        return giaDT;
+    public int getGias() {
+        return gias;
     }
 
-    public void setGiaDT(int giaDT) {
-        this.giaDT = giaDT;
+    public void setGias(int gias) {
+        this.gias = gias;
     }
 
     public int getSl() {
@@ -68,37 +68,9 @@ public class DienThoai {
         this.noiSX = noiSX;
     }
 
-    public static String getTbName() {
-        return TB_NAME;
-    }
 
-    public static String getColNameMas() {
-        return COL_NAME_MAS;
-    }
 
-    public static String getCOL_NAME_TenS() {
-        return COL_NAME_TenS;
-    }
 
-    public static String getColNameGias() {
-        return COL_NAME_GIAS;
-    }
-
-    public static String getCOL_NAME_SOLUONGs() {
-        return COL_NAME_SOLUONGs;
-    }
-
-    public static String getColNameNgaynhap() {
-        return COL_NAME_NGAYNHAP;
-    }
-
-    public static String getColNameNoisx() {
-        return COL_NAME_NOISX;
-    }
-
-    public static String getColNameMals() {
-        return COL_NAME_MALS;
-    }
 
     public static final String TB_NAME = "DienThoai";
     public static final String COL_NAME_MAS = "maDT";
